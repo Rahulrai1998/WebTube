b5a714f92ff2838a0ac10d339734e651827d20f9
Rahul Kumar <rahulrai2822march@gmail.com>
2025-05-25 19:31:58 +0530

    using cva twmerge custom btn created

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import logo from "../assets/webtube-logo.png"
 import { Menu } from 'lucide-react'
+import Button from '../components/Button'
 
 const PageHeader = () => {
     return (
         <div className='flex gap-10 lg:gap-20 justify-between'>
             <div className='flex gap-4 items-center flex-shrink-0'>
-                <button><Menu /></button>
+                <Button variant={"ghost"} size={"icon"}><Menu /></Button>
                 <a href="/"><img src={logo} /></a>
             </div>
             <div></div>
