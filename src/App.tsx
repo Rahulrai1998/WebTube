647fe6b69769ac7db86a9b50ef4202ac5785af3b
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-21 12:42:59 +0530

    chevron btn updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
+import { useState } from "react"
 import CategoryChips from "./components/CategoryChips"
 import { categories } from "./data/home"
 import PageHeader from "./layouts/PageHeader"
 
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(0)
+
+  const onSelect = (category: number) => {
+    setSelectedCategory(category)
+
+  }
+
   return (
     <div className="max-h-screen flex flex-col">
       <PageHeader />
       <div className="grid grid-cols-[auto_1fr] flex-grow overflow-auto">
         <div>SIDE BAR</div>
-        <div className="sticky top-0 bg-white z-10 pb-4">
-          <CategoryChips categories={categories} />
-        </div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryChips categories={categories} selectedCategory={selectedCategory} onSelect={onSelect} />
+          </div></div>
       </div>
     </div>
   )
--- src/components/CategoryChips.tsx
+++ src/components/CategoryChips.tsx
@@ -1,20 +1,23 @@
 import React from 'react'
 import Button from './Button'
-
-
+import { ChevronLeft } from 'lucide-react';
 
 type CategoryChipsProps = {
-    categories: string[]
+    categories: string[];
+    selectedCategory: number;
+    onSelect: (category: number) => void
 }
 
-const CategoryChips = ({ categories }: CategoryChipsProps) => {
+const CategoryChips = ({ categories, selectedCategory, onSelect }: CategoryChipsProps) => {
     return (
         <div className='overflow-x-hidden relative'>
+
             <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
-                {categories?.map((category) => <Button className='py-1 px-3 rounded-lg whitespace-nowrap' variant={"dark"}>
+                {categories?.map((category, index) => <Button onClick={() => onSelect(index)} className='py-1 px-3 rounded-lg whitespace-nowrap' variant={selectedCategory === index ? "dark" : "default"}>
                     {category}
                 </Button>)}
             </div>
+            <div className='absolute left-0 top-1/2 -translate-y-1/2'><Button variant={"ghost"} size={"icon"} className='h-full aspect-square w-auto p-1.5'><ChevronLeft /></Button></div>
         </div>
     )
 }
