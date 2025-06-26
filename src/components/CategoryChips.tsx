9f8d6263f5bb9ea9c878d61f4e15130bacf85103
Rahul Kumar <rahulrai2822march@gmail.com>
2025-06-26 11:14:05 +0530

    left translate chips

--- src/components/CategoryChips.tsx
+++ src/components/CategoryChips.tsx
@@ -11,15 +11,26 @@ type CategoryChipsProps = {
 const CategoryChips = ({ categories, selectedCategory, onSelect }: CategoryChipsProps) => {
     const [isLeftVisible, setIsLeftVisible] = useState(false)
     const [isRightVisible, setIsRightVisible] = useState(false)
+    const [translate, setTranslate] = useState(0)
+
+    const handleTranslateRight = () => {
+        setTranslate(value => {
+            const newTranslate = value - 200;
+            if (newTranslate <= 0) return 0
+            else
+                return newTranslate
+        })
+    }
+
     return (
         <div className='overflow-x-hidden relative'>
-            <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]'>
+            <div className='flex whitespace-nowrap gap-3 transition-transform w-[max-content]' style={{ transform: `translateX(-${translate}px)` }}>
                 {categories?.map((category, index) => <Button onClick={() => onSelect(index)} className='py-1 px-3 rounded-lg whitespace-nowrap' variant={selectedCategory === index ? "dark" : "default"}>
                     {category}
                 </Button>)}
             </div>
             {isLeftVisible && <div className='absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full'>
-                <Button variant={"ghost"} size={"icon"} className='h-full aspect-square w-auto p-1.5'>
+                <Button variant={"ghost"} size={"icon"} className='h-full aspect-square w-auto p-1.5' onClick={handleTranslateRight}>
                     <ChevronLeft />
                 </Button>
             </div>}
